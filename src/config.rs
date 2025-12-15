f4fde9e4e7039419e2a8a20738cc9f45514554bb
bronku <bronk.jakub@proton.me>
2025-12-15 14:16:01 +0100

    fix

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,3 @@
 pub const DEGREE: usize = 2;
 pub const MAX_KEYS: usize = DEGREE * 2 + 1;
 pub const PAGE_SIZE: usize = 4 * 16384;
-
-#[cfg(test)]
-pub mod test_config {
-    use super::*;
-
-    pub const MIN_OPS: usize = 1;
-    pub const MAX_OPS: usize = MAX_KEYS * 100;
-
-    pub const MIN_RECORDS: usize = 0;
-    pub const MAX_RECORDS: usize = MAX_KEYS * 200;
-
-    pub const PROPTEST_CASES: u32 = 200;
-}
